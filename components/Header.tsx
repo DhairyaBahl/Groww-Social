2d0858e55b355230d787eb56397cc57745b42f9f
dhairyabahl <dhairyabahl5@gmail.com>
2023-07-31 15:37:36 +0000

    Finalized UI

--- components/Header.tsx
+++ components/Header.tsx
@@ -20,7 +20,7 @@ export default function Header() {
                     />
                 </Link>
             </div>
-            <Link href="/user/navi_photography" className={styles.h786profileLink}>
+            <Link href="/user/noob_dhairya" className={styles.h786profileLink}>
                 <Image 
                     src={ProfileIcon} 
                     alt="Profile Icon of Social Seedling"
