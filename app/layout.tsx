3743e75c1b669baeb895c219051cc6074fcb7324
dhairyabahl <dhairyabahl5@gmail.com>
2023-07-31 17:50:43 +0000

    App is now Installable

--- app/layout.tsx
+++ app/layout.tsx
@@ -14,6 +14,11 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
 	<html lang="en">
+      <head>
+        <link rel="manifest" href="/manifest.json" />
+        <link rel="apple-touch-icon" href="/icon-512x512.png"></link>
+        <meta name="theme-color" content="#000" />
+      </head>
     	<body>
         <Header/>
         <Main>
