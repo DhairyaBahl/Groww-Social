a5b5337c57a1017a948d4228db8434b75f88b5c6
dhairyabahl <dhairyabahl5@gmail.com>
2023-08-01 01:06:36 +0000

    Fixed Issue with dates

--- handlers/handleDate.ts
+++ handlers/handleDate.ts
@@ -7,14 +7,23 @@ export const handleDate = (date: string) => {
     const minutes = Math.floor(seconds / 60);
     const hours = Math.floor(minutes / 60);
     const days = Math.floor(hours / 24);
-    
-    if (days > 0) {
+    const months = Math.floor(days / 30);
+    const years = Math.floor(months / 12);
+
+    if (years > 0) {
+        return `${years} years ago`;
+    } else if (months > 0) {
+        return `${months} months ago`;
+    } else if (days > 0) {
         return `${days} days ago`;
     } else if (hours > 0) {
         return `${hours} hours ago`;
     } else if (minutes > 0) {
         return `${minutes} minutes ago`;
-    } else {
+    } else if (seconds > 0) {
         return `${seconds} seconds ago`;
+    } else {
+        return 'just now';
     }
+
 };
